2fe7b079e0a3732b955d99214775b6bf78abd0de
Mikhail Puzanov <mpuzanov@ripe.net>
2017-10-16 22:04:32 +0200

    Fix buggy jetty dependencies, finish some RRDP code.

--- src/main/java/net/ripe/rpki/validator3/rrdp/RrdpService.java
+++ src/main/java/net/ripe/rpki/validator3/rrdp/RrdpService.java
@@ -1,14 +1,16 @@
 package net.ripe.rpki.validator3.rrdp;
 
+import fj.data.Either;
 import lombok.extern.slf4j.Slf4j;
 import net.ripe.rpki.commons.crypto.cms.manifest.ManifestCmsParser;
+import net.ripe.rpki.commons.crypto.cms.roa.RoaCmsParser;
 import net.ripe.rpki.commons.crypto.crl.X509Crl;
 import net.ripe.rpki.commons.crypto.x509cert.X509ResourceCertificate;
 import net.ripe.rpki.commons.crypto.x509cert.X509ResourceCertificateParser;
+import net.ripe.rpki.commons.validation.ValidationResult;
 import net.ripe.rpki.validator3.domain.RpkiObject;
 import net.ripe.rpki.validator3.domain.RpkiObjectRepository;
 import net.ripe.rpki.validator3.util.Sha256;
-import org.bouncycastle.jce.provider.X509CRLParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -35,47 +37,61 @@ public class RrdpService {
     public void storeSnapshot(final String snapshotUrl) {
         final String snapshotXml = rrdpClient.getSnapshot(snapshotUrl);
         final Snapshot snapshot = rrdpParser.snapshot(snapshotXml);
-        snapshot.asMap().forEach((uri, value) -> rpkiObjectRepository.add(createRpkiObject(uri, value.content)));
+        snapshot.asMap().forEach((uri, value) -> {
+            final Either<ValidationResult, RpkiObject> maybeRpkiObject = createRpkiObject(uri, value.content);
+            if (maybeRpkiObject.isLeft()) {
+                // TODO @mpuzanov Do something with the error, store it somewhere
+            } else {
+                rpkiObjectRepository.add(maybeRpkiObject.right().value());
+            }
+        });
     }
 
-    private RpkiObject createRpkiObject(final String uri, final byte[] content) {
+    private Either<ValidationResult, RpkiObject> createRpkiObject(final String uri, final byte[] content) {
         // TODO serialNumber must be taken from the parsed object
         try {
-            if (endsWith(uri,".cer")) {
+            if (endsWith(uri, ".cer")) {
                 final X509ResourceCertificateParser parser = new X509ResourceCertificateParser();
                 parser.parse(uri, content);
                 if (parser.isSuccess()) {
                     final X509ResourceCertificate certificate = parser.getCertificate();
-                    final byte[] sha256 = Sha256.hash(content);
-                    return new RpkiObject(Collections.singletonList(uri), certificate.getSerialNumber(), sha256, content);
+                    return Either.right(makeRpkiObject(uri, content, certificate.getSerialNumber()));
                 }
-                return null;
-            } else if (endsWith(uri,".mft")) {
+                return Either.left(parser.getValidationResult());
+            } else if (endsWith(uri, ".mft")) {
                 final ManifestCmsParser parser = new ManifestCmsParser();
                 parser.parse(uri, content);
                 if (parser.isSuccess()) {
                     final X509ResourceCertificate certificate = parser.getManifestCms().getCertificate();
-                    final byte[] sha256 = Sha256.hash(content);
-                    return new RpkiObject(Collections.singletonList(uri), certificate.getSerialNumber(), sha256, content);
+                    return Either.right(makeRpkiObject(uri, content, certificate.getSerialNumber()));
                 }
-                return null;
+                return Either.left(parser.getValidationResult());
             } else if (endsWith(uri, ".crl")) {
                 final X509Crl crl = new X509Crl(content);
-                final byte[] sha256 = Sha256.hash(content);
-                // TODO Find serial number
-                return new RpkiObject(Collections.singletonList(uri), BigInteger.ONE, sha256, content);
-
+                // TODO @mpuzanov Use proper serial number
+                BigInteger serialNumber = crl.getNumber();
+                return Either.right(makeRpkiObject(uri, content, serialNumber));
             } else if (endsWith(uri, ".roa")) {
+                final RoaCmsParser parser = new RoaCmsParser();
+                parser.parse(uri, content);
+                if (parser.isSuccess()) {
+                    final X509ResourceCertificate certificate = parser.getRoaCms().getCertificate();
+                    return Either.right(makeRpkiObject(uri, content, certificate.getSerialNumber()));
+                }
+                return Either.left(parser.getValidationResult());
+            } else if (endsWith(uri, ".gbr")) {
+                // TODO @mpuzanov Support ghost busters records
             }
 
-            final BigInteger serialNumber = BigInteger.ONE;
-            final byte[] sha256 = Sha256.hash(content);
-            return new RpkiObject(Collections.singletonList(uri), serialNumber, sha256, content);
+            return Either.left(ValidationResult.withLocation(uri).error("unknown.object", "Unknown object type"));
         } catch (IOException e) {
             log.error("Couldn't parse and store object with URI " + uri);
+            return Either.left(ValidationResult.withLocation(uri).error("parsing.object", e.getMessage()));
         }
-        // TODO Do something different
-        return null;
+    }
+
+    private RpkiObject makeRpkiObject(final String uri, final byte[] content, final BigInteger serialNumber) throws IOException {
+        return new RpkiObject(Collections.singletonList(uri), serialNumber, Sha256.hash(content), content);
     }
 
     private boolean endsWith(final String s, final String end) {
@@ -83,5 +99,6 @@ public class RrdpService {
             return false;
         return s.substring(s.length() - 3, s.length()).equalsIgnoreCase(end);
     }
+
 
 }
