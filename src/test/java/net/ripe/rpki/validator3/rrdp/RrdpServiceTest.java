20d1c5a540819fb79dc93f5bc4402a63449a20e3
Mikhail Puzanov <mpuzanov@ripe.net>
2017-10-20 14:22:12 +0200

    Extend RrdpServiceTest.

--- src/test/java/net/ripe/rpki/validator3/rrdp/RrdpServiceTest.java
+++ src/test/java/net/ripe/rpki/validator3/rrdp/RrdpServiceTest.java
@@ -17,9 +17,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.math.BigInteger;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -36,8 +39,8 @@ public class RrdpServiceTest {
     private RpkiObjects rpkiObjects;
 
     @Test
-    public void should_parse_snapshot() throws Exception {
-        TrustAnchor trustAnchor = TestObjects.newTrustAnchor();
+    public void should_parse_and_save_snapshot() throws Exception {
+        final TrustAnchor trustAnchor = TestObjects.newTrustAnchor();
         entityManager.persist(trustAnchor);
 
         final RpkiRepository rpkiRepository = new RpkiRepository(trustAnchor, "https://rrdp.ripe.net/notification.xml");
@@ -48,9 +51,18 @@ public class RrdpServiceTest {
 
         final List<RpkiObject> objects = rpkiObjects.all();
         assertEquals(3, objects.size());
+
+        final String uri1 = "rsync://rpki.ripe.net/repository/DEFAULT/61/fdce4c-2ea5-47eb-94bc-5b50ea88eeab/1/phQ5JfV8llJoaGylcrBcVa7oPfI.roa";
+        assertTrue(objects.stream().anyMatch(o -> uri1.equals(o.getLocations().first())));
+
+        final String uri2 = "rsync://rpki.ripe.net/repository/DEFAULT/a0/bf69c4-d64a-4340-9bf1-364854cbc0e8/1/Xt2pFufQkzxVnLyxgKKC8x5dVsw.mft";
+        assertTrue(objects.stream().anyMatch(o -> uri2.equals(o.getLocations().first())));
+
+        final String uri3 = "rsync://rpki.ripe.net/repository/DEFAULT/8f/db5787-c2c8-429b-8137-cbf6c1849c44/1/s70Ab2nV-TCWnoHVAM4QdNgMolQ.mft";
+        assertTrue(objects.stream().anyMatch(o -> uri3.equals(o.getLocations().first())));
     }
 
-    private static InputStream fileIS(String path) throws IOException {
+    private static InputStream fileIS(final String path) throws IOException {
         return Thread.currentThread().getContextClassLoader().getResourceAsStream(path);
     }
 
