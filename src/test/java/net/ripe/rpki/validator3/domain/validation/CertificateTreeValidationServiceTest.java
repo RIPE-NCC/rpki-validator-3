47aaf5cc127b1194d9bbad0b84460c529775e2bb
Erik Rozendaal <erozendaal@ripe.net>
2017-11-06 16:41:09 +0100

    Add child CA test for certificate tree validation

--- src/test/java/net/ripe/rpki/validator3/domain/validation/CertificateTreeValidationServiceTest.java
+++ src/test/java/net/ripe/rpki/validator3/domain/validation/CertificateTreeValidationServiceTest.java
@@ -144,6 +144,40 @@ public class CertificateTreeValidationServiceTest {
     }
 
     @Test
+    public void should_validate_child_ca() {
+        KeyPair childKeyPair = KEY_PAIR_FACTORY.generate();
+
+        TrustAnchor ta = createTrustAnchor(x -> {
+            CertificateAuthority child = CertificateAuthority.builder()
+                .dn("CN=child-ca")
+                .keyPair(childKeyPair)
+                .certificateLocation("rsync://rpki.test/CN=child-ca.cer")
+                .resources(IpResourceSet.parse("192.168.128.0/17"))
+                .notifyURI(RPKI_NOTIFY_URI)
+                .manifestURI("rsync://rpki.test/CN=child-ca/child-ca.mft")
+                .repositoryURI("rsync://rpki.test/CN=child-ca/")
+                .crlDistributionPoint("rsync://rpki.test/CN=child-ca/child-ca.crl")
+                .build();
+            x.children(Arrays.asList(child));
+        });
+        trustAnchors.add(ta);
+        RpkiRepository repository = rpkiRepositories.register(ta, RPKI_NOTIFY_URI);
+        repository.setDownloaded();
+        entityManager.flush();
+
+        subject.validate(ta.getId());
+        entityManager.flush();
+
+        List<CertificateTreeValidationRun> completed = validationRuns.findAll(CertificateTreeValidationRun.class);
+        assertThat(completed).hasSize(1);
+
+        List<Pair<CertificateTreeValidationRun, RpkiObject>> validated = rpkiObjects.findCurrentlyValidated(RpkiObject.Type.CER).collect(toList());
+        assertThat(validated).hasSize(1);
+        assertThat(validated.get(0).getLeft()).isEqualTo(completed.get(0));
+        assertThat(validated.get(0).getRight().get(X509ResourceCertificate.class, ValidationResult.withLocation("ignored.cer")).get().getSubject()).isEqualTo(new X500Principal("CN=child-ca"));
+    }
+
+    @Test
     public void should_validate_roa() {
         TrustAnchor ta = createTrustAnchor(x -> {
             x.roaPrefixes(Arrays.asList(
@@ -170,15 +204,12 @@ public class CertificateTreeValidationServiceTest {
     }
 
     private TrustAnchor createTrustAnchor(Consumer<CertificateAuthority.CertificateAuthorityBuilder> configure) {
-        KeyPair keyPair = KEY_PAIR_FACTORY.generate();
+        KeyPair rootKeyPair = KEY_PAIR_FACTORY.generate();
         CertificateAuthority.CertificateAuthorityBuilder builder = CertificateAuthority.builder()
-            .name("test trust anchor")
+            .dn("CN=test-trust-anchor")
+            .keyPair(rootKeyPair)
             .certificateLocation("rsync://rpki.test/test-trust-anchor.cer")
             .resources(IpResourceSet.parse("0.0.0.0/0"))
-            .issuerDN(TA_ISSUER_DN)
-            .subjectDN(TA_ISSUER_DN)
-            .publicKey(keyPair.getPublic())
-            .signingKey(keyPair)
             .notifyURI(RPKI_NOTIFY_URI)
             .manifestURI(TA_MANIFEST_URI)
             .repositoryURI(TA_REPOSITORY_URI)
@@ -186,63 +217,58 @@ public class CertificateTreeValidationServiceTest {
         configure.accept(builder);
         CertificateAuthority root = builder.build();
 
-        X509ResourceCertificate certificate = createCertificateAuthority(root);
+        X509ResourceCertificate certificate = createCertificateAuthority(root, root);
 
         TrustAnchor ta = new TrustAnchor();
-        ta.setName(root.name);
+        ta.setName(root.dn);
         ta.getLocations().add(root.certificateLocation);
         ta.setCertificate(certificate);
         ta.setSubjectPublicKeyInfo(X509CertificateUtil.getEncodedSubjectPublicKeyInfo(certificate.getCertificate()));
         return ta;
     }
 
-    private X509ResourceCertificate createCertificateAuthority(CertificateAuthority root) {
-        X509ResourceCertificate certificate = new X509ResourceCertificateBuilder()
-            .withResources(root.resources)
-            .withIssuerDN(new X500Principal(root.issuerDN))
-            .withSubjectDN(new X500Principal(root.subjectDN))
-            .withSubjectInformationAccess(
-                new X509CertificateInformationAccessDescriptor(X509CertificateInformationAccessDescriptor.ID_AD_RPKI_NOTIFY, URI.create(root.notifyURI)),
-                new X509CertificateInformationAccessDescriptor(X509CertificateInformationAccessDescriptor.ID_AD_RPKI_MANIFEST, URI.create(root.manifestURI))
-            )
-            .withCa(true)
-            .withKeyUsage(KeyUsage.keyCertSign | KeyUsage.cRLSign)
-            .withSerial(nextSerial())
-            .withValidityPeriod(new ValidityPeriod(Instant.now(), Instant.now().plus(Duration.standardDays(7))))
-            .withSubjectKeyIdentifier(true)
-            .withPublicKey(root.publicKey)
-            .withSigningKeyPair(root.signingKey)
-            .build();
+    private X509ResourceCertificate createCertificateAuthority(CertificateAuthority ca, CertificateAuthority issuer) {
+        ManifestCmsBuilder manifestBuilder = new ManifestCmsBuilder();
+
+        X509ResourceCertificate caCertificate = createCaCertificate(ca, ca.keyPair.getPublic(), issuer.dn, issuer.crlDistributionPoint, issuer.keyPair);
 
         X509Crl crl = new X509CrlBuilder()
-            .withIssuerDN(new X500Principal("CN=test"))
+            .withIssuerDN(caCertificate.getSubject())
             .withThisUpdateTime(DateTime.now())
             .withNextUpdateTime(DateTime.now().plusHours(8))
-            .withAuthorityKeyIdentifier(root.getPublicKey())
+            .withAuthorityKeyIdentifier(ca.keyPair.getPublic())
             .withNumber(nextSerial())
-            .build(root.signingKey.getPrivate());
-        rpkiObjects.add(new RpkiObject(TA_CRL_URI, crl));
+            .build(ca.keyPair.getPrivate());
 
-        ManifestCmsBuilder manifestBuilder = new ManifestCmsBuilder();
-        manifestBuilder.addFile("test-trust-anchor.crl", crl.getEncoded());
+        rpkiObjects.add(new RpkiObject(ca.crlDistributionPoint, crl));
+        manifestBuilder.addFile(ca.crlDistributionPoint.substring(ca.crlDistributionPoint.lastIndexOf('/') + 1), crl.getEncoded());
 
-        if (root.roaPrefixes != null) {
-            root.roaPrefixes.stream().collect(groupingBy(RoaPrefix::getAsn)).forEach((asn, roaPrefix) -> {
+        if (ca.children != null) {
+            for (CertificateAuthority child : ca.children) {
+                X509ResourceCertificate childCertificate = createCertificateAuthority(child, ca);
+
+                rpkiObjects.add(new RpkiObject(ca.repositoryURI + "/" + child.dn + ".cer", childCertificate));
+                manifestBuilder.addFile(child.dn + ".cer", childCertificate.getEncoded());
+            }
+        }
+
+        if (ca.roaPrefixes != null) {
+            ca.roaPrefixes.stream().collect(groupingBy(RoaPrefix::getAsn)).forEach((asn, roaPrefix) -> {
                 KeyPair roaKeyPair = KEY_PAIR_FACTORY.generate();
                 IpResourceSet resources = new IpResourceSet();
                 roaPrefix.stream().forEach(p -> resources.add(IpRange.parse(p.getPrefix())));
                 X509ResourceCertificate roaCertificate = new X509ResourceCertificateBuilder()
 //                    .withInheritedResourceTypes(EnumSet.allOf(IpResourceType.class))
                     .withResources(resources)
-                    .withIssuerDN(new X500Principal(root.subjectDN))
-                    .withSubjectDN(new X500Principal("CN=AS" + asn + ", CN=roa, " + root.subjectDN))
+                    .withIssuerDN(new X500Principal(ca.dn))
+                    .withSubjectDN(new X500Principal("CN=AS" + asn + ", CN=roa, " + ca.dn))
                     .withValidityPeriod(new ValidityPeriod(Instant.now(), Instant.now().plus(Duration.standardDays(1))))
                     .withPublicKey(roaKeyPair.getPublic())
-                    .withSigningKeyPair(root.signingKey)
+                    .withSigningKeyPair(ca.keyPair)
                     .withCa(false)
                     .withKeyUsage(KeyUsage.digitalSignature)
                     .withSerial(nextSerial())
-                    .withCrlDistributionPoints(URI.create(root.crlDistributionPoint))
+                    .withCrlDistributionPoints(URI.create(ca.crlDistributionPoint))
                     .build();
                 RoaCms roaCms = new RoaCmsBuilder()
                     .withAsn(new Asn(asn))
@@ -252,8 +278,8 @@ public class CertificateTreeValidationServiceTest {
                     .withCertificate(roaCertificate)
                     .withSignatureProvider(BouncyCastleProvider.PROVIDER_NAME)
                     .build(roaKeyPair.getPrivate());
-                rpkiObjects.add(new RpkiObject(root.repositoryURI + "/" + "AS" + asn + ".roa", roaCms));
 
+                rpkiObjects.add(new RpkiObject(ca.repositoryURI + "/" + "AS" + asn + ".roa", roaCms));
                 manifestBuilder.addFile("AS" + asn + ".roa", roaCms.getEncoded());
             });
         }
@@ -261,15 +287,15 @@ public class CertificateTreeValidationServiceTest {
         KeyPair manifestKeyPair = KEY_PAIR_FACTORY.generate();
         X509ResourceCertificate manifestCertificate = new X509ResourceCertificateBuilder()
             .withInheritedResourceTypes(EnumSet.allOf(IpResourceType.class))
-            .withIssuerDN(new X500Principal(root.subjectDN))
-            .withSubjectDN(new X500Principal("CN=manifest, " + root.subjectDN))
+            .withIssuerDN(caCertificate.getSubject())
+            .withSubjectDN(new X500Principal("CN=manifest, " + caCertificate.getSubject()))
             .withValidityPeriod(new ValidityPeriod(Instant.now(), Instant.now().plus(Duration.standardDays(1))))
             .withPublicKey(manifestKeyPair.getPublic())
-            .withSigningKeyPair(root.signingKey)
+            .withSigningKeyPair(ca.keyPair)
             .withCa(false)
             .withKeyUsage(KeyUsage.digitalSignature)
             .withSerial(nextSerial())
-            .withCrlDistributionPoints(URI.create(root.crlDistributionPoint))
+            .withCrlDistributionPoints(URI.create(ca.crlDistributionPoint))
             .build();
         manifestBuilder
             .withCertificate(manifestCertificate)
@@ -277,8 +303,28 @@ public class CertificateTreeValidationServiceTest {
             .withThisUpdateTime(DateTime.now())
             .withNextUpdateTime(DateTime.now().plusHours(8));
         ManifestCms manifest = manifestBuilder.build(manifestKeyPair.getPrivate());
-        rpkiObjects.add(new RpkiObject(TA_MANIFEST_URI, manifest));
-        return certificate;
+        rpkiObjects.add(new RpkiObject(ca.manifestURI, manifest));
+        return caCertificate;
+    }
+
+    private X509ResourceCertificate createCaCertificate(CertificateAuthority ca, PublicKey publicKey, String issuerDN, String crlDistributionPoint, KeyPair signingKey) {
+        return new X509ResourceCertificateBuilder()
+            .withResources(ca.resources)
+            .withIssuerDN(new X500Principal(issuerDN))
+            .withSubjectDN(new X500Principal(ca.dn))
+            .withSubjectInformationAccess(
+                new X509CertificateInformationAccessDescriptor(X509CertificateInformationAccessDescriptor.ID_AD_RPKI_NOTIFY, URI.create(ca.notifyURI)),
+                new X509CertificateInformationAccessDescriptor(X509CertificateInformationAccessDescriptor.ID_AD_RPKI_MANIFEST, URI.create(ca.manifestURI))
+            )
+            .withCrlDistributionPoints(URI.create(crlDistributionPoint))
+            .withCa(true)
+            .withKeyUsage(KeyUsage.keyCertSign | KeyUsage.cRLSign)
+            .withSerial(nextSerial())
+            .withValidityPeriod(new ValidityPeriod(Instant.now(), Instant.now().plus(Duration.standardDays(7))))
+            .withSubjectKeyIdentifier(true)
+            .withPublicKey(publicKey)
+            .withSigningKeyPair(signingKey)
+            .build();
     }
 
     protected TrustAnchor createRipeNccTrustAnchor() {
@@ -296,19 +342,17 @@ public class CertificateTreeValidationServiceTest {
     @Value
     @Builder
     public static class CertificateAuthority {
-        String name;
+        String dn;
+        KeyPair keyPair;
         String certificateLocation;
         IpResourceSet resources;
-        String issuerDN;
-        String subjectDN;
-        PublicKey publicKey;
-        KeyPair signingKey;
         String notifyURI;
         String manifestURI;
         String repositoryURI;
 
         String crlDistributionPoint;
 
+        List<CertificateAuthority> children;
         List<RoaPrefix> roaPrefixes;
     }
 }
