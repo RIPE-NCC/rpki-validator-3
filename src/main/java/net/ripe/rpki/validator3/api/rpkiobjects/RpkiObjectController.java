510d900b409636c98028265fc91aa6bca74372b1
Mikhail Puzanov <mpuzanov@ripe.net>
2017-11-14 12:23:28 +0100

    Add object JSON representation.

--- src/main/java/net/ripe/rpki/validator3/api/rpkiobjects/RpkiObjectController.java
+++ src/main/java/net/ripe/rpki/validator3/api/rpkiobjects/RpkiObjectController.java
@@ -1,25 +1,44 @@
 package net.ripe.rpki.validator3.api.rpkiobjects;
 
 import lombok.Builder;
-import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
+import net.ripe.ipresource.IpResourceSet;
+import net.ripe.rpki.commons.crypto.ValidityPeriod;
+import net.ripe.rpki.commons.crypto.cms.manifest.ManifestCms;
+import net.ripe.rpki.commons.crypto.cms.roa.RoaCms;
+import net.ripe.rpki.commons.crypto.crl.X509Crl;
+import net.ripe.rpki.commons.crypto.x509cert.X509CertificateInformationAccessDescriptor;
+import net.ripe.rpki.commons.crypto.x509cert.X509ResourceCertificate;
+import net.ripe.rpki.commons.validation.ValidationResult;
 import net.ripe.rpki.validator3.api.Api;
 import net.ripe.rpki.validator3.api.ApiResponse;
 import net.ripe.rpki.validator3.domain.CertificateTreeValidationRun;
 import net.ripe.rpki.validator3.domain.RpkiObject;
 import net.ripe.rpki.validator3.domain.RpkiObjects;
+import net.ripe.rpki.validator3.domain.ValidationCheck;
 import net.ripe.rpki.validator3.domain.ValidationRuns;
+import net.ripe.rpki.validator3.util.Hex;
+import org.apache.commons.lang3.tuple.Pair;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.ISODateTimeFormat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.constraints.NotNull;
 import java.math.BigInteger;
+import java.security.cert.X509CRLEntry;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 @RestController
 @RequestMapping(path = "/rpki-objects", produces = Api.API_MIME_TYPE)
@@ -38,67 +57,183 @@ public class RpkiObjectController {
     @GetMapping(path = "/all")
     public ResponseEntity<ApiResponse<Stream<RpkiObj>>> list() {
         final List<CertificateTreeValidationRun> vrs = validationRuns.findLatestSuccessful(CertificateTreeValidationRun.class);
-        vrs.stream().map(vr -> {
+        Stream<Set<RpkiObject>> setStream = vrs.stream().map(vr -> {
+            List<ValidationCheck> validationChecks = vr.getValidationChecks();
             final Set<RpkiObject> validatedObjects = vr.getValidatedObjects();
             return validatedObjects;
         });
 
 
-        final Stream<RpkiObj> rpkiObjStream = rpkiObjects.findCurrentlyValidated().map(p -> {
-            final CertificateTreeValidationRun vr = p.getLeft();
-            final RpkiObject ro = p.getRight();
-            switch (ro.getType()) {
+        final Stream<Pair<RpkiObject, ValidationResult>> objects = Stream.empty();
+
+        final Stream<RpkiObj> rpkiObjStream = objects.map(p -> {
+            final RpkiObject rpkiObject = p.getLeft();
+            final ValidationResult validationResult = p.getRight();
+            switch (rpkiObject.getType()) {
                 case CER:
-                    return makeCertificate(vr, ro);
+                    final Optional<X509ResourceCertificate> maybeCertificate = rpkiObject.get(X509ResourceCertificate.class, validationResult);
+                    if (maybeCertificate.isPresent()) {
+                        return makeCertificate(validationResult, maybeCertificate.get(), rpkiObject);
+                    }
+                    return null;
                 case ROA:
-                    return makeRoa(vr, ro);
+                    final Optional<RoaCms> maybeRoa = rpkiObject.get(RoaCms.class, validationResult);
+                    if (maybeRoa.isPresent()) {
+                        return makeRoa(validationResult, maybeRoa.get(), rpkiObject);
+                    }
+                    return null;
                 case MFT:
-                    return makeMft(vr, ro);
+                    final Optional<ManifestCms> maybeMft = rpkiObject.get(ManifestCms.class, validationResult);
+                    if (maybeMft.isPresent()) {
+                        return makeMft(validationResult, maybeMft.get(), rpkiObject);
+                    }
+                    return null;
                 case CRL:
-                    return makeCrl(vr, ro);
+                    final Optional<X509Crl> maybeCrl = rpkiObject.get(X509Crl.class, validationResult);
+                    if (maybeCrl.isPresent()) {
+                        return makeCrl(validationResult, maybeCrl.get(), rpkiObject);
+                    }
+                    return null;
                 default:
-                    return makeSomething(vr, ro);
+                    return makeSomething(validationResult, rpkiObject);
             }
-        });
+        }).filter(Objects::nonNull);
+
         return ResponseEntity.ok(ApiResponse.data(rpkiObjStream));
     }
 
-    private ResourceCertificate makeCertificate(CertificateTreeValidationRun vr, RpkiObject ro) {
-        return ResourceCertificate.builder().build();
+    private ResourceCertificate makeCertificate(ValidationResult validationResult, X509ResourceCertificate certificate, RpkiObject rpkiObject) {
+        return ResourceCertificate.builder().
+                uri(validationResult.getCurrentLocation().getName()).
+                valid(!validationResult.hasFailureForCurrentLocation()).
+                warnings(formatChecks(validationResult.getWarnings())).
+                errors(formatChecks(validationResult.getFailuresForAllLocations())).
+                resources(formatResources(certificate.getResources())).
+                subjectName(certificate.getSubject().getName()).
+                ski(Hex.format(certificate.getSubjectKeyIdentifier())).
+                aki(Hex.format(certificate.getAuthorityKeyIdentifier())).
+                validityTime(formatValidity(certificate.getValidityPeriod())).
+                sia(formatSia(certificate.getSubjectInformationAccess())).
+                serial(certificate.getSerialNumber()).
+                sha256(Hex.format(rpkiObject.getSha256())).
+                build();
     }
 
-    private Roa makeRoa(CertificateTreeValidationRun vr, RpkiObject ro) {
-        return Roa.builder().build();
+    private Roa makeRoa(ValidationResult validationResult, RoaCms roaCms, RpkiObject rpkiObject) {
+        return Roa.builder().
+                uri(validationResult.getCurrentLocation().getName()).
+                valid(!validationResult.hasFailureForCurrentLocation()).
+                warnings(formatChecks(validationResult.getWarnings())).
+                errors(formatChecks(validationResult.getFailuresForAllLocations())).
+                asn(roaCms.getAsn().toString()).
+                roaPrefixes(roaCms.getPrefixes().stream().map(p ->
+                        RoaPrefix.builder().maxLenght(p.getMaximumLength()).build()
+                ).collect(Collectors.toList())).
+                sha256(Hex.format(rpkiObject.getSha256())).
+                eeCertificate(makeEeCertificate(roaCms.getCertificate())).
+                build();
     }
 
-    private Mft makeMft(CertificateTreeValidationRun vr, RpkiObject ro) {
-        return Mft.builder().build();
+    private EeCertificate makeEeCertificate(X509ResourceCertificate certificate) {
+        return EeCertificate.builder().
+                validityTime(formatValidity(certificate.getValidityPeriod())).
+                subjectName(certificate.getSubject().getName()).
+                serial(certificate.getSerialNumber()).
+                resources(formatResources(certificate.getResources())).
+                build();
     }
 
-    private Crl makeCrl(CertificateTreeValidationRun vr, RpkiObject ro) {
-        return Crl.builder().build();
+    private Mft makeMft(ValidationResult validationResult, ManifestCms manifestCms, RpkiObject rpkiObject) {
+        return Mft.builder().
+                uri(validationResult.getCurrentLocation().getName()).
+                valid(!validationResult.hasFailureForCurrentLocation()).
+                warnings(formatChecks(validationResult.getWarnings())).
+                errors(formatChecks(validationResult.getFailuresForAllLocations())).
+                eeCertificate(makeEeCertificate(manifestCms.getCertificate())).
+                thisUpdateTime(formatDateTime(manifestCms.getThisUpdateTime())).
+                nextUpdateTime(formatDateTime(manifestCms.getNextUpdateTime())).
+                manifestNumber(manifestCms.getNumber()).
+                entries(makeMftEntries(manifestCms.getFiles())).
+                sha256(Hex.format(rpkiObject.getSha256())).
+                build();
     }
 
-    private RpkiObj makeSomething(CertificateTreeValidationRun vr, RpkiObject ro) {
+    private List<MftEntry> makeMftEntries(final Map<String, byte[]> files) {
+        return files.entrySet().stream().map(e ->
+                MftEntry.builder().filename(e.getKey()).sha256(Hex.format(e.getValue())).build()
+        ).collect(Collectors.toList());
+    }
+
+    private Crl makeCrl(ValidationResult validationResult, X509Crl crl, RpkiObject rpkiObject) {
+        return Crl.builder().
+                uri(validationResult.getCurrentLocation().getName()).
+                valid(!validationResult.hasFailureForCurrentLocation()).
+                warnings(formatChecks(validationResult.getWarnings())).
+                errors(formatChecks(validationResult.getFailuresForAllLocations())).
+                aki(Hex.format(crl.getAuthorityKeyIdentifier())).
+                serial(crl.getNumber()).
+                revocations(crl.getCrl().getRevokedCertificates().stream().map(X509CRLEntry::getSerialNumber).collect(Collectors.toList())).
+                build();
+    }
+
+    private RpkiObj makeSomething(ValidationResult vr, RpkiObject ro) {
         return new RpkiObj();
     }
 
+    private static List<String> formatResources(IpResourceSet resources) {
+        return StreamSupport.stream(resources.spliterator(), false).map(Object::toString).collect(Collectors.toList());
+    }
+
+    private static Map<String, String> formatSia(X509CertificateInformationAccessDescriptor[] sia) {
+        return Arrays.stream(sia).collect(Collectors.toMap(
+                s -> s.getMethod().toString(),
+                s -> s.getLocation().toASCIIString()
+        ));
+    }
+
+    private static ValidityTime formatValidity(ValidityPeriod validityPeriod) {
+        final DateTimeFormatter formatter = ISODateTimeFormat.basicDateTime();
+        return ValidityTime.builder().
+                notValidAfter(formatDateTime(validityPeriod.getNotValidAfter())).
+                notValidBefore(formatDateTime(validityPeriod.getNotValidBefore())).
+                build();
+    }
+
+    private static String formatDateTime(final DateTime dateTime) {
+        return dateTime == null ? null : dateTime.toString(ISODateTimeFormat.basicDateTime());
+    }
+
+    private static List<Issue> formatChecks(final List<net.ripe.rpki.commons.validation.ValidationCheck> checks) {
+        return checks.stream().map(validationCheck -> Issue.builder().
+                status(validationCheck.getStatus().toString()).
+                key(validationCheck.getKey()).
+                parameters(validationCheck.getParams()).
+                build()).collect(Collectors.toList());
+    }
+
+
     static class RpkiObj {
+    }
+
+    @Builder
+    private static class ValidityTime extends RpkiObj {
+        private String notValidBefore;
+        private String notValidAfter;
     }
 
     @Builder
     private static class ResourceCertificate extends RpkiObj {
         private String uri;
         private boolean valid;
-        private String[] warnings;
-        private String[] errors;
+        private List<Issue> warnings;
+        private List<Issue> errors;
 
-        private String[] resources;
+        private List<String> resources;
         private String subjectName;
         private String ski;
         private String aki;
-        private String validityTime;
-        private String sia;
+        private ValidityTime validityTime;
+        private Map<String, String> sia;
         // TODO There're three serials in the story
         private BigInteger serial;
         private String sha256;
@@ -106,15 +241,14 @@ public class RpkiObjectController {
 
     @Builder
     static class EeCertificate {
-        private String[] resources;
+        private List<String> resources;
         private String subjectName;
-        private String validityTime;
+        private ValidityTime validityTime;
         private BigInteger serial;
     }
 
     @Builder
     static class RoaPrefix {
-        private String asn;
         private String prefix;
         private int maxLenght;
     }
@@ -123,11 +257,12 @@ public class RpkiObjectController {
     private static class Roa extends RpkiObj {
         private String uri;
         private boolean valid;
-        private String[] warnings;
-        private String[] errors;
+        private List<Issue> warnings;
+        private List<Issue> errors;
 
+        private String asn;
         private EeCertificate eeCertificate;
-        private RoaPrefix roaPrefix;
+        private List<RoaPrefix> roaPrefixes;
         private String sha256;
     }
 
@@ -135,13 +270,13 @@ public class RpkiObjectController {
     static class Mft extends RpkiObj {
         private String uri;
         private boolean valid;
-        private String[] warnings;
-        private String[] errors;
+        private List<Issue> warnings;
+        private List<Issue> errors;
 
         private EeCertificate eeCertificate;
         private String thisUpdateTime;
         private String nextUpdateTime;
-        private int manifestNumber;
+        private BigInteger manifestNumber;
         private List<MftEntry> entries;
         private String sha256;
     }
@@ -156,12 +291,19 @@ public class RpkiObjectController {
     static class Crl extends RpkiObj {
         private String uri;
         private boolean valid;
-        private String[] warnings;
-        private String[] errors;
+        private List<Issue> warnings;
+        private List<Issue> errors;
 
         private String aki;
-        private String revocations;
+        private List<BigInteger> revocations;
         private BigInteger serial;
+    }
+
+    @Builder
+    static class Issue {
+        private String status;
+        private String key;
+        private String[] parameters;
     }
 
     /*
