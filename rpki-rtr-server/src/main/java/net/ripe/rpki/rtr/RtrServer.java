796f453a8cbfda77255269dd126a439e8d1ed8af
Mikhail Puzanov <mpuzanov@ripe.net>
2018-01-15 10:27:01 +0100

    Remove test code.

--- rpki-rtr-server/src/main/java/net/ripe/rpki/rtr/RtrServer.java
+++ rpki-rtr-server/src/main/java/net/ripe/rpki/rtr/RtrServer.java
@@ -86,15 +86,6 @@ public class RtrServer {
 
         @Override
         public void channelRead(ChannelHandlerContext ctx, Object msg) {
-            ByteBuf in = (ByteBuf) msg;
-            try {
-                while (in.isReadable()) { // (1)
-                    System.out.print((char) in.readByte());
-                    System.out.flush();
-                }
-            } finally {
-                ReferenceCountUtil.release(msg); // (2)
-            }
             final ErrorPdu errorPdu = ErrorPdu.of(ErrorCode.NoDataAvailable, new byte[]{}, "not implemented");
             final ByteBuf buf = ctx.alloc().buffer(errorPdu.length());
             errorPdu.write(buf);
