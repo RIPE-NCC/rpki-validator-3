904cbff00ff3c53c9a47de149acce593a06c40ec
Mikhail Puzanov <mpuzanov@ripe.net>
2018-01-29 13:06:08 +0100

    Add initial BGP-related code, fix license headers.

---
+++ rpki-validator/src/main/java/net/ripe/rpki/validator3/api/roas/ValidatedRoasController.java
@@ -0,0 +1,85 @@
+/**
+ * The BSD License
+ *
+ * Copyright (c) 2010-2018 RIPE NCC
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   - Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   - Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *   - Neither the name of the RIPE NCC nor the names of its contributors may be
+ *     used to endorse or promote products derived from this software without
+ *     specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.ripe.rpki.validator3.api.roas;
+
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
+import net.ripe.rpki.validator3.api.Api;
+import net.ripe.rpki.validator3.api.ApiResponse;
+import net.ripe.rpki.validator3.domain.RpkiObject;
+import net.ripe.rpki.validator3.domain.RpkiObjects;
+import net.ripe.rpki.validator3.domain.TrustAnchor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.stream.Stream;
+
+@RestController
+@RequestMapping(path = "/roas", produces = Api.API_MIME_TYPE)
+@Slf4j
+public class ValidatedRoasController {
+    @Autowired
+    private RpkiObjects rpkiObjects;
+
+    @GetMapping(path = "/")
+    public ResponseEntity<ApiResponse<Stream<RoaPrefix>>> list() {
+        final Stream<RoaPrefix> validatedPrefixes = rpkiObjects
+                .findCurrentlyValidated(RpkiObject.Type.ROA)
+                .flatMap(pair -> {
+                            @NotNull @Valid TrustAnchor trustAnchor = pair.getKey().getTrustAnchor();
+                            return pair.getValue().getRoaPrefixes().stream()
+                                    .map(prefix -> new RoaPrefix(
+                                            String.valueOf(prefix.getAsn()),
+                                            prefix.getPrefix(),
+                                            prefix.getEffectiveLength(),
+                                            trustAnchor.getName()
+                                    ));
+                        }
+                )
+                .distinct();
+
+        return ResponseEntity.ok(ApiResponse.<Stream<RoaPrefix>>builder()
+                .data(validatedPrefixes)
+                .build());
+    }
+
+    @Value
+    public static class RoaPrefix {
+        private String asn;
+        private String prefix;
+        private int maxLength;
+        private String trustAnchor;
+    }
+}
