2d361bf5f6bacf81a667dbc0b8abe68a15d1a7cb
Tim Bruijnzeels <tim@ripe.net>
2018-02-13 12:23:41 +0100

    Adding generic builds and start/stop scripts etc for the RPKI validator and RTR server

---
+++ rpki-validator/src/main/resources/packaging/generic/bin/rpki-validator-3.sh
@@ -0,0 +1,197 @@
+#!/usr/bin/env bash
+#
+# The BSD License
+#
+# Copyright (c) 2010-2018 RIPE NCC
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#   - Redistributions of source code must retain the above copyright notice,
+#     this list of conditions and the following disclaimer.
+#   - Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions and the following disclaimer in the documentation
+#     and/or other materials provided with the distribution.
+#   - Neither the name of the RIPE NCC nor the names of its contributors may be
+#     used to endorse or promote products derived from this software without
+#     specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+
+EXECUTION_DIR=`dirname "$BASH_SOURCE"`
+cd ${EXECUTION_DIR}
+CONFIG_FILE="conf/application.properties"
+JAR="lib/rpki-validator-3.jar"
+LOG_FILE="log/rpki-validator-3.log"
+
+APP_NAME="rpki-validator"
+PID_FILE=${APP_NAME}.pid
+
+function error_exit {
+    echo -e "[ error ] $1"
+    exit 1
+}
+
+function info {
+    echo -e "[ info ] $1"
+}
+
+function warn {
+    echo -e "[ warn ] $1"
+}
+
+function usage {
+cat << EOF
+Usage: $0 start
+   or  $0 stop
+   or  $0 restart
+   or  $0 status
+EOF
+}
+
+
+function check_java_version {
+  JAVA_VERSION=`${JAVA_CMD} -version 2>&1 | grep version | sed 's/.* version //g'`
+  MAJOR_VERSION=`echo ${JAVA_VERSION} | sed 's/"\([[:digit:]]\)\.\([[:digit:]]\).*"/\1\2/g'`
+  if (( ${MAJOR_VERSION} < 18 )) ; then
+    error_exit "RPKI validator requires Java 1.8 or greater, your version of java is ${JAVA_VERSION}";
+  fi
+}
+
+#
+# Specify the location of the Java home directory. If set then $JAVA_CMD will
+# be defined to $JAVA_HOME/bin/java
+#
+if [ -d "${JAVA_HOME}"  ] ; then
+    JAVA_CMD="${JAVA_HOME}/bin/java"
+else
+    warn "JAVA_HOME is not set, will try to find java on path."
+    JAVA_CMD=`which java`
+fi
+
+if [ -z $JAVA_CMD ]; then
+    error_exit "Cannot find java on path. Make sure Java 8 or higher is installed and/or set JAVA_HOME"
+fi
+
+check_java_version
+
+# See how we're called
+FIRST_ARG="$1"
+shift
+if [[ -n $MODE ]]; then
+   usage
+   exit
+fi
+
+#Validate that rsync is available in the path and is executable
+if ! [ -x "$(command -v rsync)" ]; then
+  echo 'rsync not found. It is necessary to sync repositories.' >&2
+  exit 1
+fi
+
+
+function parse_config_line {
+    local CONFIG_KEY=$1
+    local VALUE=`grep "^$CONFIG_KEY" $CONFIG_FILE | sed 's/#.*//g' | awk -F "=" '{ print $2 }'`
+
+    if [ -z $VALUE ]; then
+        error_exit "Cannot find value for: $CONFIG_KEY in config-file: $CONFIG_FILE"
+    fi
+    eval "$2=$VALUE"
+}
+
+parse_config_line "jvm.memory.initial" JVM_XMS
+parse_config_line "jvm.memory.maximum" JVM_XMX
+
+MEM_OPTIONS="-Xms$JVM_XMS -Xmx$JVM_XMX"
+
+#
+# Determine if the application is already running
+#
+function is_running {
+    if [ -e ${PID_FILE} ]; then
+        if [ x`cat ${PID_FILE}` == x`pgrep -f -- -Dapp.name=${APP_NAME}` ]; then
+            echo "true";
+            exit;
+        fi
+    fi
+    echo "false";
+}
+
+function start_validator {
+    RUNNING=$(is_running)
+    if [ ${RUNNING} == "true" ]; then
+        error_exit "${APP_NAME} is already running"
+    fi
+
+    info "Starting ${APP_NAME}..."
+    info "writing logs under log directory"
+    ${JAVA_CMD} ${MEM_OPTIONS} -Dapp.name="${APP_NAME}" -Dspring.config.location="file:${CONFIG_FILE}" -jar "${JAR}" >> ${LOG_FILE} &
+
+    PID=$!
+    echo $PID > $PID_FILE
+    info "Writing PID ${PID} to ${PID_FILE}"
+}
+
+function stop_validator {
+    info "Stopping ${APP_NAME}..."
+    RUNNING=$(is_running)
+    if [ ${RUNNING} == "true" ]; then
+        kill `cat ${PID_FILE}` && rm ${PID_FILE}
+    else
+        info "${APP_NAME} is not running"
+    fi
+}
+
+function check_status {
+    RUNNING=$(is_running)
+    if [ ${RUNNING} == "true" ]; then
+        info "${APP_NAME} is running"
+    else
+        info "${APP_NAME} is not running"
+    fi
+    exit 0
+}
+
+function restart_validator {
+    stop_validator
+    RUNNING=$(is_running)
+    while [ $RUNNING == "true" ]; do
+        sleep 1
+        echo "Waiting for validator to stop..."
+        RUNNING=$(is_running)
+    done
+    start_validator
+}
+
+case ${FIRST_ARG} in
+    start)
+        start_validator
+        ;;
+    stop)
+        stop_validator
+        ;;
+    restart)
+        restart_validator
+        ;;
+    status)
+        check_status
+        ;;
+    *)
+        usage
+        exit
+        ;;
+esac
+
+exit $?
