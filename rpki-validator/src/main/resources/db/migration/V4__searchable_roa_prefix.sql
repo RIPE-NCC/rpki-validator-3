28939ca18f649560f4e84aff6149d18579d87e44
Mikhail Puzanov <mpuzanov@ripe.net>
2018-02-14 17:57:36 +0100

    Add index for the prefix.

--- rpki-validator/src/main/resources/db/migration/V4__searchable_roa_prefix.sql
+++ rpki-validator/src/main/resources/db/migration/V4__searchable_roa_prefix.sql
@@ -4,3 +4,5 @@ ALTER TABLE rpki_object_roa_prefixes ADD COLUMN prefix_family TINYINT NOT NULL;
 ALTER TABLE rpki_object_roa_prefixes ADD COLUMN prefix_begin DECIMAL(39, 0) NOT NULL;
 ALTER TABLE rpki_object_roa_prefixes ADD COLUMN prefix_end DECIMAL(39, 0) NOT NULL;
 ALTER TABLE rpki_object_roa_prefixes ADD CONSTRAINT prefix_family_4_or_6 CHECK prefix_family IN (4, 6);
+
+CREATE INDEX rpki_object_roa_prefixes__prefix_idx ON rpki_object_roa_prefixes (prefix_begin, prefix_end, prefix_family);
