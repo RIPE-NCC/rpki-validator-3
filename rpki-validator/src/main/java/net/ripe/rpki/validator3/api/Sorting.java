d1f4a02cd128a24c245860990f5fbae04f992d27
Mikhail Puzanov <mpuzanov@ripe.net>
2018-02-16 14:05:44 +0100

    Refactor paging-related code.

--- rpki-validator/src/main/java/net/ripe/rpki/validator3/api/Sorting.java
+++ rpki-validator/src/main/java/net/ripe/rpki/validator3/api/Sorting.java
@@ -27,7 +27,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-package net.ripe.rpki.validator3.api.roas;
+package net.ripe.rpki.validator3.api;
 
 import lombok.Data;
 
