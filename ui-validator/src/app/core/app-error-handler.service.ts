82493adf8be52462833d6f335dbff41466a39609
Ivana Svonja <isvonja@ripe.net>
2018-02-25 10:20:01 +0100

    UI ErrorHandler extends instead of implement to get back original errors

--- ui-validator/src/app/core/app-error-handler.service.ts
+++ ui-validator/src/app/core/app-error-handler.service.ts
@@ -2,12 +2,14 @@ import {ErrorHandler, Injectable, Injector} from "@angular/core";
 import {Router} from "@angular/router";
 
 @Injectable()
-export class ApplicationErrorHandler implements ErrorHandler {
+export class ApplicationErrorHandler extends ErrorHandler {
 
   constructor(private injector: Injector) {
+    super();
   }
 
   handleError(error) {
+    super.handleError(error);
     const router = this.injector.get(Router);
     router.navigate(['error', {error: error.status, errorMsg: error.statusText}] );
   }
