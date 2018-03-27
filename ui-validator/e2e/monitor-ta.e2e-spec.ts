3b16ee50433edd85b5aec10f7bdc380563c23edf
Ivana Svonja <isvonja@ripe.net>
2018-03-27 15:55:09 +0200

    UI - added e2e test for monitoring page

--- ui-validator/e2e/monitor-ta.e2e-spec.ts
+++ ui-validator/e2e/monitor-ta.e2e-spec.ts
@@ -1,4 +1,4 @@
-import { AppPage } from './app.po';
+import {AppPage} from './app.po';
 import {MonitorTaPage} from "./pages/MonitorTaPage";
 
 describe('Monitor TA Page', () => {
@@ -13,4 +13,38 @@ describe('Monitor TA Page', () => {
     monitorTaPage
       .expectTitleHeaderToBe('Overview for RIPE NCC RPKI Root');
   });
-});
+
+  it('should display correctly Summery of page', () => {
+    monitorTaPage
+      .expectSummerySubtitleToBe('Summary')
+      .expectNumberOfSummaryTableRowsToBe(4)
+      .expectCanValidatTrustAnchorToBe('Yes')
+      .expectLastCompletedValidationToBe('2018-02-25 04:32:31')
+      .expectProcessedItemsToHave()
+      .expectGreenFlagInRowOfSummary(3, '20932')
+      .expectOrangeMutedFlagInRowOfSummary(3)
+      .expectRedMutedFlagInRowOfSummary(3)
+      .expectRepositoriesToHave()
+      .expectGreenFlagInRowOfSummary(4, '91')
+      .expectOrangeMutedFlagInRowOfSummary(4) //tooltip
+      .expectRedFlagInRowOfSummary(4, '1');
+  });
+
+  it('should display correctly Validation Issues of page', () => {
+    monitorTaPage
+      .expectValidationIssuesSubtitleToBe('Validation Issues')
+      .expectNumberOfValidationIssuesTableRowsToBe(2)
+      .moveMouseOverColumn(1, 1)
+      .expectPopoverOnHover(1, 1)//popover value
+      .expectErrorFlagInRowOfValidationDetails(1)
+      .expectWarningFlagInRowOfValidationDetails(2);
+  });
+
+  it('should display correctly Repositories of page', () => {
+    monitorTaPage
+      .expectRepositoriesSubtitleToBe('Repositories')
+      .expectNumberOfRepositoriesTableRowsToBe(2)
+      .expectSuccessFlagInRowOfRepositories(1)
+      .expectErrorFlagInRowOfRepositories(2);
+  });
+})
--- ui-validator/e2e/pages/MonitorTaPage.ts
+++ ui-validator/e2e/pages/MonitorTaPage.ts
@@ -11,4 +11,113 @@ export class MonitorTaPage {
     return this;
   }
 
+  expectSummerySubtitleToBe(title: string) {
+    expect(element(by.css('#summary h3')).getText()).toEqual(title);
+    return this;
+  }
+
+  expectNumberOfSummaryTableRowsToBe(rows: number) {
+    expect(element.all(by.css('#summary table tbody tr')).count()).toBe(rows);
+    return this;
+  }
+
+  expectCanValidatTrustAnchorToBe(value: string) {
+    expect(element(by.css('#summary table tr:nth-child(1) td:nth-child(1)')).getText()).toEqual('Trust Anchor could be validated');
+    // green flag
+    expect(element(by.css('#summary table tr:nth-child(1) .alert-success')).getText()).toEqual(value);
+    return this;
+  }
+
+  expectLastCompletedValidationToBe(date: string) {
+    expect(element(by.css('#summary table tr:nth-child(2) td:nth-child(1)')).getText()).toEqual('Last completed validation');
+    expect(element(by.css('#summary table tr:nth-child(2) td:nth-child(2)')).getText()).toEqual(date);
+    return this;
+  }
+
+  expectProcessedItemsToHave() {
+    expect(element(by.css('#summary table tr:nth-child(3) td:nth-child(1)')).getText()).toEqual('Processed Items');
+    return this;
+  }
+
+  expectRepositoriesToHave() {
+    expect(element(by.css('#summary table tr:nth-child(4) td:nth-child(1)')).getText()).toEqual('Repositories');
+    return this;
+  }
+
+  expectGreenFlagInRowOfSummary(row: number, content: string) {
+    expect(element(by.css(`#summary table tbody tr:nth-child(${row}) flag[ng-reflect-color=green] .alert`)).getText()).toBe(content);
+    return this;
+  }
+
+  expectRedFlagInRowOfSummary(row: number, content: string) {
+    expect(element(by.css(`#summary table tbody tr:nth-child(${row}) flag[ng-reflect-color=red] .alert`)).getText()).toBe(content);
+    return this;
+  }
+
+  expectOrangeMutedFlagInRowOfSummary(row: number) {
+    expect(element(by.css(`#summary table tbody tr:nth-child(${row}) flag[ng-reflect-color="orange"] .alert-warning-muted`)).getText()).toBe('0');
+    return this;
+  }
+
+  expectRedMutedFlagInRowOfSummary(row: number) {
+    expect(element(by.css(`#summary table tbody tr:nth-child(${row}) flag[ng-reflect-color="red"] .alert-danger-muted`)).getText()).toBe('0');
+    return this;
+  }
+
+  // VALIDATION ISSUES
+  expectValidationIssuesSubtitleToBe(title: string) {
+    expect(element(by.css('validation-details h3')).getText()).toEqual(title);
+    return this;
+  }
+
+  expectNumberOfValidationIssuesTableRowsToBe(rows: number) {
+    expect(element.all(by.css('validation-details table tbody tr')).count()).toBe(rows);
+    return this;
+  }
+
+  expectPopoverToHave(row: number, column: number, expectToSee: boolean) {
+    expect(element(by.css(`validation-details table tbody tr:nth-child(${row}) td:nth-child(${column}) ngb-popover-window`)).isPresent()).toBe(expectToSee);
+    return this;
+  }
+
+  expectPopoverOnHover(row: number, column: number) {
+    expect(element(by.css(`validation-details table tbody tr:nth-child(${row}) td:nth-child(${column}) ngb-popover-window`)).isDisplayed()).toBe(true);
+    return this;
+  }
+
+  moveMouseOverColumn(row: number, column: number) {
+    browser.actions().mouseMove(element(by.css(`validation-details table tbody tr:nth-child(${row}) td:nth-child(${column}) img`))).perform();
+    return this;
+  }
+
+  expectErrorFlagInRowOfValidationDetails(row: number) {
+    expect(element(by.css(`validation-details table tbody tr:nth-child(${row}) flag .alert-danger`)).getText()).toBe('ERROR');
+    return this;
+  }
+
+  expectWarningFlagInRowOfValidationDetails(row: number) {
+    expect(element(by.css(`validation-details table tbody tr:nth-child(${row}) flag .alert-warning`)).getText()).toBe('WARNING');
+    return this;
+  }
+
+  // REPOSITORIES
+  expectRepositoriesSubtitleToBe(title: string) {
+    expect(element(by.css('repositories h3')).getText()).toEqual(title);
+    return this;
+  }
+
+  expectNumberOfRepositoriesTableRowsToBe(rows: number) {
+    expect(element.all(by.css('repositories table tbody tr')).count()).toBe(rows);
+    return this;
+  }
+
+  expectSuccessFlagInRowOfRepositories(row: number) {
+    expect(element(by.css(`repositories table tbody tr:nth-child(${row}) flag .alert-success`)).getText()).toBe('DOWNLOADED');
+    return this;
+  }
+
+  expectErrorFlagInRowOfRepositories(row: number) {
+    expect(element(by.css(`repositories table tbody tr:nth-child(${row}) flag .alert-danger`)).getText()).toBe('FAILED');
+    return this;
+  }
 }
