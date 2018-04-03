61394c5bf24db29086044e2b618422836c5db048
Ivana Svonja <isvonja@ripe.net>
2018-04-03 10:31:52 +0200

    UI - removed useless import

--- ui-validator/src/app/core/error.component.ts
+++ ui-validator/src/app/core/error.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit, Output} from '@angular/core'
+import {Component, OnInit} from '@angular/core'
 import {ActivatedRoute} from "@angular/router";
 import {TranslateService} from "@ngx-translate/core";
 
