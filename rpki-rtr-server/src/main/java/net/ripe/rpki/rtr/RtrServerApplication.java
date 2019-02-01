afc7f4ee7b0df5df9043e91fc713ddbae374a2de
Mikhail Puzanov <mpuzanov@ripe.net>
2019-02-01 13:18:54 +0100

    Remove DataSource, we don't a DB.

--- rpki-rtr-server/src/main/java/net/ripe/rpki/rtr/RtrServerApplication.java
+++ rpki-rtr-server/src/main/java/net/ripe/rpki/rtr/RtrServerApplication.java
@@ -31,14 +31,19 @@ package net.ripe.rpki.rtr;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		DataSourceAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class
+})
 @EnableScheduling
 @Slf4j
 public class RtrServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RtrServerApplication.class, args);
 	}
