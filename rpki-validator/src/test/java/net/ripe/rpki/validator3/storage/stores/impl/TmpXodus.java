6c6ae096f8c3c57a26bc977bec89d548c98ff2b8
Mikhail Puzanov <mpuzanov@ripe.net>
2019-06-05 15:40:45 +0200

    Add test Xodus environment.

---
+++ rpki-validator/src/test/java/net/ripe/rpki/validator3/storage/stores/impl/TmpXodus.java
@@ -0,0 +1,79 @@
+/**
+ * The BSD License
+ *
+ * Copyright (c) 2010-2018 RIPE NCC
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   - Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   - Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *   - Neither the name of the RIPE NCC nor the names of its contributors may be
+ *     used to endorse or promote products derived from this software without
+ *     specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.ripe.rpki.validator3.storage.stores.impl;
+
+import jetbrains.exodus.env.Environment;
+import jetbrains.exodus.env.EnvironmentConfig;
+import jetbrains.exodus.env.Environments;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import net.ripe.rpki.validator3.storage.xodus.Xodus;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PreDestroy;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Profile("test")
+@Component
+@Slf4j
+public class TmpXodus extends Xodus {
+
+    @Getter
+    private Environment env;
+
+    private final Path mdb;
+
+    public TmpXodus() throws IOException {
+        mdb = Files.createTempDirectory("test-rpki-validator-");
+        File file = mdb.toFile();
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+        env = open();
+    }
+
+    private Environment open() {
+        final EnvironmentConfig config = new EnvironmentConfig()
+                .setLogDurableWrite(true)
+                .setEnvGatherStatistics(true)
+                .setLogCacheUseNio(true);
+
+        return Environments.newInstance(mdb.toFile(), config);
+    }
+
+    @PreDestroy
+    public void close() {
+        getEnv().close();
+    }
+}
