ec45d25d362bf9263293f6b5220e23769da76e38
Mikhail Puzanov <misha.puzanov@gmail.com>
2019-06-18 10:35:44 +0200

    Change hex to lower-case.

--- rpki-validator/src/test/java/net/ripe/rpki/validator3/rrdp/RrdpParserTest.java
+++ rpki-validator/src/test/java/net/ripe/rpki/validator3/rrdp/RrdpParserTest.java
@@ -55,17 +55,17 @@ public class RrdpParserTest {
         final String uri1 = "rsync://bandito.ripe.net/repo/3a87a4b1-6e22-4a63-ad0f-06f83ad3ca16/default/671570f06499fbd2d6ab76c4f22566fe49d5de60.mft";
         DeltaElement e1 = delta.asMap().get(uri1);
         assertEquals(uri1, ((DeltaPublish)e1).uri);
-        assertEquals("226AB8CD3C887A6EBDDDF317F2FAFC9CF3EFC5D43A86347AC0FEFFE4DC0F607E", Hex.format(((DeltaPublish)e1).getHash().get()));
+        assertEquals("226ab8cd3c887a6ebdddf317f2fafc9cf3efc5d43a86347ac0feffe4dc0f607e", Hex.format(((DeltaPublish)e1).getHash().get()));
 
         final String uri2 = "rsync://bandito.ripe.net/repo/3a87a4b1-6e22-4a63-ad0f-06f83ad3ca16/default/671570f06499fbd2d6ab76c4f22566fe49d5de60.crl";
         DeltaElement e2 = delta.asMap().get(uri2);
         assertEquals(uri2, ((DeltaPublish)e2).uri);
-        assertEquals("2B551A6C10CCA04C174B0CEB3B64652A5534D1385BEAA40A55A68CB06055E6BB", Hex.format(((DeltaPublish)e2).getHash().get()));
+        assertEquals("2b551a6c10cca04c174b0ceb3b64652a5534d1385beaa40a55a68cb06055e6bb", Hex.format(((DeltaPublish)e2).getHash().get()));
 
         final String uri3 = "rsync://bandito.ripe.net/repo/3a87a4b1-6e22-4a63-ad0f-06f83ad3ca16/default/example.roa";
         DeltaElement e3 = delta.asMap().get(uri3);
         assertEquals(uri3, ((DeltaWithdraw)e3).uri);
-        assertEquals("2B551A6C10CCA04C174B0CEB3B64652A5534D1385BEAA40A55A68CB06055E6BB", Hex.format(((DeltaWithdraw) e3).getHash()));
+        assertEquals("2b551a6c10cca04c174b0ceb3b64652a5534d1385beaa40a55a68cb06055e6bb", Hex.format(((DeltaWithdraw) e3).getHash()));
     }
 
     @Test
@@ -73,7 +73,7 @@ public class RrdpParserTest {
         final Notification notification = new RrdpParser().notification(fileIS("rrdp/notification1.xml"));
         assertEquals("9df4b597-af9e-4dca-bdda-719cce2c4e28", notification.sessionId);
         assertEquals("http://repo.net/repo/snapshot.xml", notification.snapshotUri);
-        assertEquals("EEEA7F7AD96D85BBD1F7274FA7DA0025984A2AF3D5A0538F77BEC732ECB1B068", notification.snapshotHash);
+        assertEquals("eeea7f7ad96d85bbd1f7274fa7da0025984a2af3d5a0538f77bec732ecb1b068", notification.snapshotHash);
         assertEquals(BigInteger.ONE, notification.serial);
         assertEquals(0, notification.deltas.size());
     }
